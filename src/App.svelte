7ba3e1d72b7f5b555e8dd42186cc1d958e1ea4a3
fengkx <liangkx8237@gmail.com>
2019-10-22 23:32:07 +0800

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<svelte:options tag="buggy-compoment" />
+<script>
+	import '@fortawesome/fontawesome-free/js/all.js';
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<p>
+    <a>FontAwesome icon inside compoment<i class="fab fa-js"></i></a>
+</p>
